c8e0dbf7d811471938fa45faf89d5fb963782074
Shashidhar Basavaraj Nagaral <shashinagaral123@gmail.com>
2023-04-19 22:58:58 +0530

    initial commit

---
+++ SplitwiseExpenseService/src/main/java/com/splitwise/expenseservice/SplitwiseExpenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.splitwise.expenseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseExpenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseExpenseServiceApplication.class, args);
+	}
+
+}
---
+++ SplitwiseUserService/src/main/java/com/splitwise/userservice/SplitwiseUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.splitwise.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitwiseUserServiceApplication.class, args);
+	}
+
+}
