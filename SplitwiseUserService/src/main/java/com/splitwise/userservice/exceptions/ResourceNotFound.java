b4d20ccfb801cf0a1127fa8e1d1ccdcba4f5c995
Jayaa Shree Laxmi Kishoore <jayashreelaxmik1998@gmail.com>
2023-05-14 15:43:44 +0530

    added all logger related code

--- SplitwiseUserService/src/main/java/com/splitwise/userservice/exceptions/ResourceNotFound.java
+++ SplitwiseUserService/src/main/java/com/splitwise/userservice/exceptions/ResourceNotFound.java
@@ -1,10 +1,17 @@
 package com.splitwise.userservice.exceptions;
 
+import com.splitwise.userservice.controller.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ResourceNotFound extends RuntimeException{
+
+    private static final Logger logger = LoggerFactory.getLogger(ResourceNotFound.class);
     String resourceName;
     String fieldName;
     public ResourceNotFound(String resourceName,String fieldName) {
         super(String.format("%s: %s not found",resourceName,fieldName ));
+        logger.error("'{}' ${}$ #{}# *{}* #{}# - Resource not Found","","","","","");
         this.fieldName=fieldName;
         this.resourceName=resourceName;
     }
